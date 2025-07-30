ca593698f5626c93e8f505d1975dfb656c6d5c53
ajreitz2024 <ajreitz2024@gmail.com>
2025-07-30 09:59:38 -0400

    Added "half" function for if the input is an integer and added comments to the function. Also added code in the main function to test the integer version of this function.

--- Project14/Unit8PartB.cpp
+++ Project14/Unit8PartB.cpp
@@ -11,8 +11,18 @@ T half(T num) {
 	return (num / 2); // return the value divided by 2
 }
 
+int half(int num) { // if the data type is int, use this version of the function
+	float newNum = static_cast<float>(num); // turn value of num into a float and assign it to newNum
+	newNum = newNum / 2; // divide by 2 to find half
+	newNum = round(newNum); // round number
+	num = static_cast<int>(newNum); // set the final value of newNum as an int and assign it to num
+	return num; // return the final value of num
+}
+
 int main() {
 	double a = 7.0;
+	int c = 3;
 	cout << half(a) << endl;
+	cout << half(c) << endl;
 	return 0;
 }
