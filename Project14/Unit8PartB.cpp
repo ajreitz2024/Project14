c1f38a66643a98bfc29755ddb79da49966095db1
ajreitz2024 <ajreitz2024@gmail.com>
2025-07-30 09:52:09 -0400

    Added template function that returns half the value of the input and added code in the main function to test the function.

--- Project14/Unit8PartB.cpp
+++ Project14/Unit8PartB.cpp
@@ -6,5 +6,13 @@
 
 using namespace std;
 
+template <typename T>
+T half(T num) {
+	return (num / 2); // return the value divided by 2
+}
+
 int main() {
+	double a = 7.0;
+	cout << half(a) << endl;
+	return 0;
 }
